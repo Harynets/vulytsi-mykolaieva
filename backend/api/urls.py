f942fc3902a748e203eb51b3d3d493bfdb60a07d
Anton <garynets.anton@gmail.com>
2025-10-04 18:00:51 +0300

    Add API endpoint to retrieve all streets

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns=[
     path("street/<str:name>", views.StreetRetrieve.as_view(), name="street_retrieve"),
+    path("street_list/", views.StreetList.as_view(), name="street_list"),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,14 @@
 from rest_framework import generics
 from .models import Street
-from .serializers import StreetSerializer
+from .serializers import StreetSerializer, StreetNameSerializer
+
 
 class StreetRetrieve(generics.RetrieveAPIView):
     queryset = Street.objects.all()
     serializer_class = StreetSerializer
     lookup_field = "name"
+
+
+class StreetList(generics.ListAPIView):
+    queryset = Street.objects.all()
+    serializer_class = StreetNameSerializer
