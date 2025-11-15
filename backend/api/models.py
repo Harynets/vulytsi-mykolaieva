0123565db0f528b658be2c2969fabd1a80519b4c
Anton <garynets.anton@gmail.com>
2025-11-15 21:47:40 +0200

    Add category field for the Person model

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,6 +2,19 @@ from django.db import models
 
 
 class Person(models.Model):
+    CATEGORY_CHOICES = [
+        ("military", "Військова справа"),
+        ("politics", "Політика"),
+        ("science", "Наука"),
+        ("business", "Бізнес"),
+        ("culture", "Культура"),
+        ("writers", "Письменницька діяльність"),
+        ("sport", "Спорт"),
+        ("public", "Громадська діяльність"),
+        ("religion", "Релігія"),
+        ("other", "Інше")
+    ]
+
     name = models.CharField(max_length=120)
     date_of_birth = models.DateField(null=True, blank=True)
     date_of_death = models.DateField(null=True, blank=True)
@@ -9,6 +22,7 @@ class Person(models.Model):
     short_biography = models.TextField(blank=True)
     biography = models.TextField(blank=True)
     quote = models.CharField(max_length=320, blank=True)
+    category = models.CharField(max_length=30, choices=CATEGORY_CHOICES)
 
     def __str__(self):
         return self.name
