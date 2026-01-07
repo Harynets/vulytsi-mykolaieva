fcf1a4f0c5c89c3700f5f57c5d0799f3fc45af23
Anton <garynets.anton@gmail.com>
2026-01-07 20:06:20 +0200

    Add message when no biographies are found

--- frontend/src/app/biographies/components/BiographiesList.tsx
+++ frontend/src/app/biographies/components/BiographiesList.tsx
@@ -1,11 +1,20 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React from "react";
 import PersonCard from "./PersonCard";
 import { BiographiesListProps } from "@/app/types/BiographiesListProps";
 
 function BiographiesList({ filteredOptions, personArr }: BiographiesListProps) {
-    if (filteredOptions.length < 0) {
-        return;
+    if (filteredOptions.length === 0) {
+        return (
+            <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                <Typography variant="h5" sx={{ textAlign: "center" }}>
+                    За Вашим запитом нічого не знайдено.
+                </Typography>
+                <Typography variant="h6" sx={{ textAlign: "center" }}>
+                    Можливо Ви ввели некоректний запит. Перевірте правильність написання ключових слів.
+                </Typography>
+            </Box>
+        );
     }
     return (
         <Box sx={{ display: "flex", flexWrap: "wrap" }}>
