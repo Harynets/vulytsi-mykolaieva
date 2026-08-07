1716b1bb960c4fc2cf68cf679ac6247fcc5a7bb7
Anton <garynets.anton@gmail.com>
2026-08-07 16:04:55 +0300

    Update hero section styling and background image

--- frontend/src/app/components/Hero.tsx
+++ frontend/src/app/components/Hero.tsx
@@ -7,23 +7,44 @@ function Hero() {
         <Box
             sx={{
                 width: "100%",
-                py: 24,
+                py: 22,
                 px: 5,
+                marginBottom: 0,
                 backgroundImage: "url('/background_map.png')",
                 backgroundPosition: "center",
                 backgroundSize: "cover",
                 backgroundRepeat: "no-repeat",
             }}
         >
-            <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                <Typography sx={{ textAlign: "center" }} variant="h3" gutterBottom>
-                    Відкрий історії людей, чиї імена живуть у назвах вулиць міста
+            <Box
+                sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                }}
+            >
+                <Typography
+                    sx={{ textAlign: "center", fontSize: { xs: 36, sm: 40, md: 42, lg: 44 } }}
+                    fontWeight="bold"
+                    variant="h3"
+                    gutterBottom
+                >
+                    Дізнайтесь історії людей, чиї імена носять вулиці Миколаєва
                 </Typography>
-                <Typography sx={{ textAlign: "center" }} variant="h4" color="text.secondary" gutterBottom>
-                    Інтерактивна карта з біографіями відомих постатей
+                <Typography
+                    sx={{ textAlign: "center", marginBottom: "64px", fontSize: { xs: 26, md: 28 }, fontWeight: "bold" }}
+                    variant="h4"
+                    color="text.secondary"
+                    gutterBottom
+                >
+                    Інтерактивна карта з
+                    <Box component="span" sx={{ color: "#2E7D32" }}>
+                        &nbsp;біографіями&nbsp;
+                    </Box>
+                    відомих постатей
                 </Typography>
 
-                <Button href="map" variant="contained" color="success" startIcon={<TravelExploreIcon />}>
+                <Button href="map" variant="contained" size="large" color="success" startIcon={<TravelExploreIcon />}>
                     Відкрити карту
                 </Button>
             </Box>
