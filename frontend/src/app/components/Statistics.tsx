d640a438532d9568012a7841401f5bfec749277b
Anton <garynets.anton@gmail.com>
2026-08-08 12:55:02 +0300

    Add icons for the Statistics component

--- frontend/src/app/components/Statistics.tsx
+++ frontend/src/app/components/Statistics.tsx
@@ -1,14 +1,24 @@
 import { Box, Typography, Zoom } from "@mui/material";
 import React from "react";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import SignpostOutlinedIcon from "@mui/icons-material/SignpostOutlined";
 
 function Statistics() {
     const statisticsData = [
-        { name: "Вулиць", number: "888" },
-        { name: "Біографії", number: "194" },
-        { name: "Років історії міста", number: "236" },
+        { name: "Вулиць", number: "888", icon: <SignpostOutlinedIcon color="success" /> },
+        { name: "Біографії", number: "194", icon: <PersonOutlineOutlinedIcon color="success" /> },
+        { name: "Років історії міста", number: "236", icon: <AccessTimeIcon color="success" /> },
     ];
+
     return (
-        <Box sx={{ display: "flex", justifyContent: "space-around", marginY: "85px" }}>
+        <Box
+            sx={{
+                display: "flex",
+                justifyContent: "space-around",
+                paddingY: { xs: "95px", md: "120px" },
+            }}
+        >
             {statisticsData.map((data, index) => {
                 return (
                     <Zoom key={index} in={true} {...{ timeout: 600 * (index + 1) }}>
@@ -26,19 +36,23 @@ function Statistics() {
                             >
                                 {data.number}
                             </Typography>
-                            <Typography
-                                variant="h5"
-                                color="text.secondary"
-                                sx={{
-                                    fontSize: {
-                                        xs: "14px",
-                                        md: "20px",
-                                        lg: "24px",
-                                    },
-                                }}
-                            >
-                                {data.name}
-                            </Typography>
+                            <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                                {data.icon}
+                                <Typography
+                                    variant="h5"
+                                    color="text.secondary"
+                                    sx={{
+                                        fontSize: {
+                                            xs: "14px",
+                                            md: "20px",
+                                            lg: "24px",
+                                        },
+                                        marginLeft: "6px",
+                                    }}
+                                >
+                                    {data.name}
+                                </Typography>
+                            </Box>
                         </Box>
                     </Zoom>
                 );
