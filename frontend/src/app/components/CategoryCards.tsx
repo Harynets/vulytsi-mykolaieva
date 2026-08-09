a043dda7ca3b635c30a320500f4b360f0cdea295
Anton <garynets.anton@gmail.com>
2026-08-09 13:42:34 +0300

    Change card layout and add color for icons

--- frontend/src/app/components/CategoryCards.tsx
+++ frontend/src/app/components/CategoryCards.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import { Box, Paper, Slide, Typography } from "@mui/material";
+import { Box, Paper, Slide, Typography, useMediaQuery } from "@mui/material";
 import HistoryEduIcon from "@mui/icons-material/HistoryEdu";
 import MilitaryTechIcon from "@mui/icons-material/MilitaryTech";
-import GavelIcon from "@mui/icons-material/Gavel";
 import ScienceIcon from "@mui/icons-material/Science";
 import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
 import PaletteIcon from "@mui/icons-material/Palette";
@@ -11,20 +10,167 @@ import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
 import GroupsIcon from "@mui/icons-material/Groups";
 import ChurchIcon from "@mui/icons-material/Church";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
 import { useEffect, useRef, useState } from "react";
+import Link from "next/link";
 
 function CategoryCards() {
-    const labelAndIcon = [
-        { label: "Військова справа", icon: <MilitaryTechIcon sx={{ fontSize: 140 }} /> },
-        { label: "Політика", icon: <GavelIcon sx={{ fontSize: 140 }} /> },
-        { label: "Наука", icon: <ScienceIcon sx={{ fontSize: 140 }} /> },
-        { label: "Бізнес", icon: <BusinessCenterIcon sx={{ fontSize: 140 }} /> },
-        { label: "Культура", icon: <PaletteIcon sx={{ fontSize: 140 }} /> },
-        { label: "Письменницька діяльність", icon: <HistoryEduIcon sx={{ fontSize: 140 }} /> },
-        { label: "Спорт", icon: <EmojiEventsIcon sx={{ fontSize: 140 }} /> },
-        { label: "Громадська діяльність", icon: <GroupsIcon sx={{ fontSize: 140 }} /> },
-        { label: "Релігія", icon: <ChurchIcon sx={{ fontSize: 140 }} /> },
-        { label: "Інше", icon: <MoreHorizIcon sx={{ fontSize: 140 }} /> },
+    const isMobile = useMediaQuery("(max-width:600px)");
+
+    const FONT_SIZE = isMobile ? 60 : 110;
+    const STROKE_WIDTH = 0.5;
+
+    const categoryData = [
+        {
+            label: "Військова справа",
+            icon: (
+                <MilitaryTechIcon
+                    sx={{
+                        fontSize: FONT_SIZE,
+                        color: "#B0B47B",
+                        stroke: "#464A2B",
+                        strokeWidth: STROKE_WIDTH,
+                        strokeLinejoin: "round",
+                    }}
+                />
+            ),
+            color: "#757A49",
+        },
+        {
+            label: "Політика",
+            icon: (
+                <AccountBalanceIcon
+                    sx={{
+                        fontSize: FONT_SIZE,
+                        color: "#90A2C4",
+                        stroke: "#102B45",
+                        strokeWidth: STROKE_WIDTH,
+                        strokeLinejoin: "round",
+                    }}
+                />
+            ),
+            color: "#1F517F",
+        },
+        {
+            label: "Наука",
+            icon: (
+                <ScienceIcon
+                    sx={{
+                        fontSize: FONT_SIZE,
+                        color: "#F6B589",
+                        stroke: "#8F4911",
+                        strokeWidth: STROKE_WIDTH,
+                        strokeLinejoin: "round",
+                    }}
+                />
+            ),
+            color: "#e0843a",
+        },
+        {
+            label: "Бізнес",
+            icon: (
+                <BusinessCenterIcon
+                    sx={{
+                        fontSize: FONT_SIZE,
+                        color: "#A6B0BF",
+                        stroke: "#3A4752",
+                        strokeWidth: STROKE_WIDTH,
+                        strokeLinejoin: "round",
+                    }}
+                />
+            ),
+            color: "#6A7F90",
+        },
+        {
+            label: "Культура",
+            icon: (
+                <PaletteIcon
+                    sx={{
+                        fontSize: FONT_SIZE,
+                        color: "#C9A4CF",
+                        stroke: "#4F2C55",
+                        strokeWidth: STROKE_WIDTH,
+                        strokeLinejoin: "round",
+                    }}
+                />
+            ),
+            color: "#8D5197",
+        },
+        {
+            label: "Письменницька діяльність",
+            icon: (
+                <HistoryEduIcon
+                    sx={{
+                        fontSize: FONT_SIZE,
+                        color: "#D99491",
+                        stroke: "#5C2D2B",
+                        strokeWidth: STROKE_WIDTH,
+                        strokeLinejoin: "round",
+                    }}
+                />
+            ),
+            color: "#9D5C5A",
+        },
+        {
+            label: "Спорт",
+            icon: (
+                <EmojiEventsIcon
+                    sx={{
+                        fontSize: FONT_SIZE,
+                        color: "#EAC463",
+                        stroke: "#73551A",
+                        strokeWidth: STROKE_WIDTH,
+                        strokeLinejoin: "round",
+                    }}
+                />
+            ),
+            color: "#CA9A37",
+        },
+        {
+            label: "Громадська діяльність",
+            icon: (
+                <GroupsIcon
+                    sx={{
+                        fontSize: FONT_SIZE,
+                        color: "#C9E7E7",
+                        stroke: "#4F7F7A",
+                        strokeWidth: STROKE_WIDTH,
+                        strokeLinejoin: "round",
+                    }}
+                />
+            ),
+            color: "#99D0CA",
+        },
+        {
+            label: "Релігія",
+            icon: (
+                <ChurchIcon
+                    sx={{
+                        fontSize: FONT_SIZE,
+                        color: "#F3ECD9",
+                        stroke: "#8F836D",
+                        strokeWidth: STROKE_WIDTH,
+                        strokeLinejoin: "round",
+                    }}
+                />
+            ),
+            color: "#EDE1C9",
+        },
+        {
+            label: "Інше",
+            icon: (
+                <MoreHorizIcon
+                    sx={{
+                        fontSize: FONT_SIZE,
+                        color: "#DFE1E4",
+                        stroke: "#686A6E",
+                        strokeWidth: STROKE_WIDTH,
+                        strokeLinejoin: "round",
+                    }}
+                />
+            ),
+            color: "#CACBCE",
+        },
     ];
 
     const BoxRef = useRef<HTMLDivElement | null>(null);
@@ -41,7 +187,7 @@ function CategoryCards() {
                     }
                 });
             },
-            { threshold: 0.5 }
+            { threshold: 0.3 },
         );
 
         if (BoxRef.current) observer.observe(BoxRef.current);
@@ -50,8 +196,8 @@ function CategoryCards() {
     }, []);
 
     return (
-        <>
-            <Typography variant="h4" sx={{ marginTop: "36px" }} gutterBottom>
+        <Box sx={{ marginY: "200px" }}>
+            <Typography sx={{ fontSize: { xs: 28, md: 32, lg: 34 } }} variant="h4" gutterBottom>
                 Досліджуйте біографії за категоріями
             </Typography>
             <Box
@@ -63,7 +209,7 @@ function CategoryCards() {
                     marginBottom: "36px",
                 }}
             >
-                {labelAndIcon.map((elem, index) => {
+                {categoryData.map((elem, index) => {
                     return (
                         <Slide
                             direction="right"
@@ -75,28 +221,46 @@ function CategoryCards() {
                             <Paper
                                 elevation={3}
                                 sx={{
-                                    ":hover": {
-                                        boxShadow: 10,
+                                    "&:hover": {
+                                        boxShadow: 8,
+                                        scale: 1.01,
                                     },
                                     width: { xs: "47%", lg: "18%" },
                                     display: "flex",
                                     flexDirection: "column",
-                                    alignItems: "center",
                                     justifyContent: "center",
                                     marginBottom: "40px",
                                     cursor: "pointer",
+                                    padding: "10px",
+                                    borderBottom: `14px solid ${elem.color}`,
                                 }}
                             >
-                                {elem.icon}
-                                <Typography variant="h6" sx={{ marginTop: "16px", marginBottom: "6px", textAlign: "center" }}>
-                                    {elem.label}
-                                </Typography>
+                                <Link
+                                    href={`/biographies?page=1&categories=${elem.label}`}
+                                    style={{
+                                        width: "100%",
+                                        height: "100%",
+                                        display: "flex",
+                                        flexDirection: "column",
+                                        justifyContent: "start",
+                                        alignItems: "center",
+                                        marginTop: "12px",
+                                    }}
+                                >
+                                    <Box sx={{ backgroundColor: elem.color, borderRadius: "50%", padding: "30px" }}>
+                                        {elem.icon}
+                                    </Box>
+
+                                    <Typography variant="h6" sx={{ marginTop: "16px", marginBottom: "6px", textAlign: "center" }}>
+                                        {elem.label}
+                                    </Typography>
+                                </Link>
                             </Paper>
                         </Slide>
                     );
                 })}
             </Box>
-        </>
+        </Box>
     );
 }
 
